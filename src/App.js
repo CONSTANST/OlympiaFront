5c8fd3094875610a4706eb6d05f1901926084286
Thibault <thibault.constans@outlook.com>
2023-03-02 20:57:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 // import {useEffect, useState} from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./page/home";
+import Signup from "./page/Signup";
+import Login from "./page/Login";
 import TicketBooK from "./page/ticketsBook";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
         <Route path="/ticketsBook/:id" element={<TicketBooK />} />
       </Routes>
     </Router>
