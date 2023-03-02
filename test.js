7756d434a8f8d16edf749390f6b1f0e48cd36ea5
Thibault <thibault.constans@outlook.com>
2023-03-02 13:29:38 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// calendier via package gerer les date sous format date gere sous moongose
+// type date sur moogose
+// si en string
+// bouton admins/master
+// deux navigation composant route pour user et composant route pour les autres
+// "nested routing"?
+// bien attention au token et status. comment cherche le role en question?
+// si cookies durée de vie courte
+// gettime natif js millisecond
+// getTime()
+// https://github.com/CONSTANST/OlympiaBack.git
